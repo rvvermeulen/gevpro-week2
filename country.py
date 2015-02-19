241c99a65d41eb462a24b764b188e2372d9c4c10
S2746549 <s2746549@lett-edu-176-225.let.rug.nl>
2015-02-19 11:41:44 +0100

    main() functie toegevoegd om Country() class te testen

--- country.py
+++ country.py
@@ -2,10 +2,23 @@
 #country.py
 #Roos Vermeulen
 
+import sys
+
 class Country:
 	
 	def __init__(self, name):
 		self.name = name
 
 	def __str__(self):
-		print("Hello from " + self.name)
+		return "Hello from " + self.name
+	
+def main(argv):
+    if len(argv) >= 2:
+        country = Country(argv[1])
+        print("\n" + str(country) + "\n")
+    else:
+        print("Usage: country.py Arg1, voer voor Arg1 een string in.")
+        exit(-1)
+
+if __name__=="__main__":
+    main(sys.argv)
